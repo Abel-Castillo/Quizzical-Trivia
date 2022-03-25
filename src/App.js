c948b98edabc31cc383eb3ab5ea251d53190260f
Abel Castillo <abel.castillo101@gmail.com>
2022-03-24 21:06:57 -0700

    added buttons to questions page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useState} from 'react'
 import uniqid from 'uniqid';
 import './index.css';
 import QuizCover from './components/QuizCover'
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,15 @@
 export default function Questions (props){
     const {question} = props
+    const typeBoolean = <div><button>True</button><button>False</button></div>
+    const typeMultiple = [...question.incorrect_answers,question.correct_answer ]
+    console.log(typeMultiple)
     return (
         <div>
-            <p>{question.question}</p>
+            {/* used dangerouslySetInnerHTML to be able to render text without the unicode*/}
+            <p dangerouslySetInnerHTML={{ __html: question.question }}></p>
+
+            {question.type === 'boolean' && typeBoolean}
+            {question.type === 'multiple' && typeMultiple.map(answer => <button>{answer}</button>)}
         </div>
     )
 }
