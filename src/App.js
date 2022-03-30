c60a37a60b5ba36e6fb040edd882cadd0296a643
Abel Castillo <abel.castillo101@gmail.com>
2022-03-29 21:34:39 -0700

    updated handle answers to be able to choose an answer

--- src/App.js
+++ src/App.js
@@ -5,28 +5,44 @@ import QuizCover from './components/QuizCover'
 import Questions from './components/Questions'
 
 function App() {
+  // use coverPage state to hide/show
   const [coverPage, setCoverPage] = useState(false)
+  // initialitze the questions
   const [questions, setQuestions] = useState(fetchQuestions)
+  // save the answers input
+  const [answers, setAnswers] = useState([])
   console.log(questions)
-  
+  // get the questions from API 
   async function fetchQuestions () {
     let data = await fetch('https://opentdb.com/api.php?amount=10')
     .then(response => response.json()).then(data => data.results)
-     data = data.map(d=> ({...d,id:uniqid()}))
+     data = data.map(d=> ({...d,id:uniqid(),selectedAnswer:''}))
       
     setQuestions(data)
+    // return statement to prevent error leak
     return data
   }
   
   function startQuiz (){
+    // update the true or false coverPage
     setCoverPage(true)
   }
+  function handleAnswers (e, id) {
+    // Copy the questions state to modify the matching id's
+    const qst = [...questions]
+    const questionNumber = questions.findIndex(q => q.id === id)
+    // if the id's match update the selectedAnswer with the choosen answer
+    qst[questionNumber].selectedAnswer= e.target.name
+    // update the state with the new modified questions array
+    setQuestions(qst)
 
+    }
+  console.log(answers)
   return (
     <div className="App">
       {/* if coverPage is false show coverPage else display the questions */}
       {!coverPage? <QuizCover startQuiz={startQuiz}/> : 
-      questions.map(question => <Questions question={question} key={question.id}/>)}
+      questions.map(question => <Questions question={question} key={question.id} handleAnswers={handleAnswers}/>)}
       
     </div>
   );
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,15 +1,23 @@
 export default function Questions (props){
-    const {question} = props
-    const typeBoolean = <div><button>True</button><button>False</button></div>
+    const {question, handleAnswers} = props
+    const typeBoolean = <div>
+                <input type='button' value='True' name='true' onClick={(event)=>handleAnswers(event,question.id)}/>
+                <input type='button' value='False' name='false' onClick={(event)=>handleAnswers(event,question.id)}/>
+            </div>  
+        
     const typeMultiple = [...question.incorrect_answers,question.correct_answer ]
-    console.log(typeMultiple)
+   
     return (
         <div>
             {/* used dangerouslySetInnerHTML to be able to render text without the unicode*/}
             <p dangerouslySetInnerHTML={{ __html: question.question }}></p>
-
             {question.type === 'boolean' && typeBoolean}
-            {question.type === 'multiple' && typeMultiple.map(answer => <button>{answer}</button>)}
+            {question.type === 'multiple' && typeMultiple.map(answer => 
+                <input type='button' 
+                    value={answer} 
+                    name={answer} 
+                    className={answer === question.selectedAnswer && 'answer--selected'}
+                    onClick={(event)=>handleAnswers(event,question.id)}/>)}
         </div>
     )
 }
